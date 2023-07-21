f6915e0f7b7b7d5a6a929c3dc667fd550c679811
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-07-21 01:34:42 -0300

    cambio de raiz de login button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { UserHomeComponent } from './components/user-home/user-home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginButtonComponent } from './components/user-home/login-button/login-button.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
 import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
